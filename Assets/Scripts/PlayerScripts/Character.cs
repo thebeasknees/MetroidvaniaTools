d3405b3aa63740553db36c81e86f9af9f6bdc2a5
Keven Beasley <76821317+thebeasknees@users.noreply.github.com>
2021-05-03 13:02:39 -0500

    Added jump scene to player

--- Assets/Scripts/PlayerScripts/Character.cs
+++ Assets/Scripts/PlayerScripts/Character.cs
@@ -8,6 +8,8 @@ namespace MetroidvaniaTools
     {
         [HideInInspector]
         public bool isFacingLeft;
+        [HideInInspector]
+        public bool isGrounded;
 
         protected Collider2D col;
         protected Rigidbody2D rb;
@@ -37,6 +39,20 @@ namespace MetroidvaniaTools
             {
                 transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
             }
+        }
+
+        protected virtual bool CollisionCheck(Vector2 direction, float distance, LayerMask collision)
+        {
+            RaycastHit2D[] hits = new RaycastHit2D[10];
+            int numHits = col.Cast(direction, hits, distance);
+            for(int i = 0;i < numHits;i++)
+            {
+                if((1 << hits[i].collider.gameObject.layer & collision) != 0)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
